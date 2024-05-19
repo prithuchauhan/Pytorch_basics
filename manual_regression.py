039d5c4c02998a5fa205884469644c2eaa499bd0
Prithu <132932509+prithuchauhan@users.noreply.github.com>
2024-05-19 14:45:19 +0530

    Update manual_regression.py

--- manual_regression.py
+++ manual_regression.py
@@ -6,11 +6,13 @@ X = np.array([1,2,3,4] , dtype=np.float32)
 Y = np.array([2,4,6,8] , dtype=np.float32)
 
 w = 0.0
+#b = 0.0 
 
 # model prediction 
 
 def forward(x):
     return w*x
+    #return w*x+b
 
 # loss = MSE
 # MSE = 1/N * (w*x - y)**2
@@ -23,7 +25,10 @@ def loss(y,y_hat):
 # gradient = 1/N 2x (yhat-y) // calculated mathematically dL/dw
 
 def gradient(x,y,y_hat):
-    return np.dot(2*x,(y_hat-y)).mean()  
+    return np.dot(2*x,(y_hat-y)).mean() 
+    #dw = np.dot(2*x,(y_hat-y)).mean()
+    #db = (2*(y_hat-y)).mean()
+    #return dw,db
 
 initial_prediction = forward(5)
 
@@ -44,11 +49,13 @@ for epoch in range(epochs):
     
     # gradients at each node
     dw = gradient(X,Y,y_pred)
+    #dw,db = gradient(X,Y,y_pred)
     
     
     
     #update weights // in gradient descent optimization u update the weight in negative direction of gradient
     w -= lr*dw
+    #db = -lr*db
 
     
     if (epoch%2==0):
